6234a663e2546e165381ea57256b3126cf7ea307
dshestakova <darya.shestakova@idt.net>
2021-04-15 15:10:09 +0300

    third commit

--- src/main/java/apiClients/AbstractClient.java
+++ src/main/java/apiClients/AbstractClient.java
@@ -14,20 +14,20 @@ public abstract class AbstractClient implements BaseApiClient{
 
     @Override
     public <T> BaseRequest get(String url, Map<String, String> paths, Map<String, String> queries, Map<String, String> headers, Class<T> clazz){
-        Object response = buildRequest(url, paths, queries, headers, null);
-        return doRequest(response, Method.GET, clazz);
+        Object request = buildRequest(url, paths, queries, headers, null);
+        return doRequest(request, Method.GET, clazz);
     }
 
     @Override
     public <T> BaseRequest put(String url, Map<String, String> paths, Map<String, String> queries, Map<String, String> headers, T jsonObject, Class<T> clazz){
-        Object response = buildRequest(url, paths, queries, headers, jsonObject);
-        return doRequest(response, Method.PUT, clazz);
+        Object request = buildRequest(url, paths, queries, headers, jsonObject);
+        return doRequest(request, Method.PUT, clazz);
     }
 
     @Override
     public <T> BaseRequest delete(String url, Map<String, String> paths, Map<String, String> queries, Map<String, String> headers, Class<T> clazz){
-        Object response = buildRequest(url, paths, queries, headers, null);
-        return doRequest(response, Method.DELETE, clazz);
+        Object request = buildRequest(url, paths, queries, headers, null);
+        return doRequest(request, Method.DELETE, clazz);
     }
 
     public abstract Object buildRequest(String url, Map<String, String> paths, Map<String, String> queries, Map<String,
